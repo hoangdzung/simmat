ad9f5a2af85ae6335fffc6b2699a19f63f7bf448
hoang dzung <dungmin97@gmail.com>
2019-09-08 00:28:42 +0700

    first commit

---
+++ algo.py
@@ -0,0 +1,82 @@
+import numpy as np 
+import scipy.optimize.linear_sum_assignment as lsa 
+from utils import getLemmas, match, path_similarity
+
+def matchIdenticalPhrases(L1, L2):
+    P = set()
+    considered1 = set()
+    considered2 = set()
+    while(True):
+        new = ''
+        pos = None 
+        for i in range(len(L1)):
+            for j in range(len(L2)):
+                if i not in considered1 and j not in considered2:
+                    temp = match(L1, L2, i, j)
+                    if len(temp) > len(new):
+                        new = temp
+                        pos = (i,j)
+        if len(new) > 0:
+            P.add(new)
+            considered1 = considered1.union(set(range(pos[0],pos[0]+len(new))))
+            considered2 = considered2.union(set(range(pos[1],pos[1]+len(new))))
+            print(new, considered1,considered2)
+        else:
+            break
+    
+    return P, considered1, considered2
+
+def matching(L1, L2):
+    sim_matrix = np.zeros((len(L1), len(L2)))
+    for i, w1 in enumerate(L1):
+        for j, w2 in enumerate(L2):
+            sim_matrix[i][j] = path_similarity(w1, w2)
+
+    row_ind, col_ind = lsa(-sim_matrix)
+    return sim_matrix[row_ind, col_ind]
+    
+def relmat(N, M, alpha=0.2):
+    numerator = sum([len(i) + len(i)**alpha for i in N])
+    denominator = numerator
+
+    numerator += sum([i**alpha for i in M])
+    denominator += 2*len(M)
+
+    return numerator/denominator
+
+def p(rew1, rew2):
+    p = abs(rew1 - rew2) / max(rew1, rew2)
+    p = 0.5*(p**3)
+    return  p 
+
+def simmat(s1, s2, alpha=0.2):
+    lemma1, pos1 = getLemmas(s1)
+    lemma2, pos2 = getLemmas(s2)
+
+    ### Phase 1
+    P, considered1, considered2 = matchIdenticalPhrases(lemma1, lemma2)
+
+    lemma1 = [lemma1[i] for i in range(len(lemma1)) if i not in considered1]
+    lemma2 = [lemma2[i] for i in range(len(lemma2)) if i not in considered2]
+
+    pos1 = [pos1[i] for i in range(len(pos1)) if i not in considered1]
+    pos2 = [pos2[i] for i in range(len(pos2)) if i not in considered2]
+
+    ### Phase 2 
+    lemma1 = [lemma1[i] for i in range(len(lemma1)) if pos1[i] not in ['IN', 'PRP$', 'MD', '.']]
+    lemma2 = [lemma2[i] for i in range(len(lemma2)) if pos2[i] not in ['IN', 'PRP$', 'MD', '.']]
+
+    ### Phase 3 
+    match_pairs = matching(lemma1, lemma2)
+
+    ### Phase 4
+    rel_mat = relmat(P, match_pairs, alpha)
+
+    simmat = rel_mat * (1-p(len(lemma1) - len(match_pairs), len(lemma2) - len(match_pairs)))
+
+    return simmat
+    
+if __name__ == '__main__':
+    sen1 = input("Sentence 1:")
+    sen2 = input("Sentence 2:")
+    print(simmat(sen1, sen2))
---
+++ utils.py
@@ -0,0 +1,25 @@
+import stanfordnlp 
+import wordnet
+nlp = stanfordnlp.Pipeline(processors = "tokenize,mwt,lemma,pos")
+
+
+def getLemmas(sentence):
+    doc = nlp(sentence)
+    return [word.lemma for word in doc.sentences[0].words], [word.xpos for word in doc.sentences[0].words] 
+
+def path_similarity(w1, w2):
+    sims = []
+    for i in wordnet.synsets(w1):
+        for j in wordnet.synsets(w2):
+            sims.append(i.path_similarity(j))
+
+    return max([i for i in sims if i])
+
+def match(L1, L2, i, j):
+    end1 = i 
+    end2 = j
+    while (end1 < len(L1) and end2 < len(L2) and L1[end1]==L2[end2]):
+        end1+=1
+        end2+=1 
+
+    return tuple(L1[i:end1])
