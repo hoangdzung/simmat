355f6451d84a203cc1d288f7b75c47773776a206
hoang dzung <dungmin97@gmail.com>
2019-10-07 11:14:15 +0700

    add train val evalutaion

--- main.py
+++ main.py
@@ -29,5 +29,25 @@ for line in tqdm(open(filename).readlines()):
         pred_probs.append(simmat(sen1, sen2))
     else:
         pred_probs.append(get_sim(sen1, sen2))
+
 print(roc_auc_score(np.array(true_labels), np.array(pred_probs)))
+
+from sklearn.model_selection import  KFold
+from sklearn.linear_model import LogisticRegression
+
+X = np.expand_dims(pred_probs,-1)
+y = np.array(true_labels)
+
+kf = KFold(n_splits=5)
+scores = []
+for train_index, test_index in kf.split(X): 
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+
+    clf = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial').fit(X_train, y_train)
+    score = clf.score(X_test, y_test)
+    scores.append(score)
+
+print(np.mean(np.array(scores)))
+
 np.save('pred_'+mode+'.npy', np.array(pred_probs))
